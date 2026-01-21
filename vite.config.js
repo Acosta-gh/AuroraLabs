ff3fdf5501e72fb2a4733f2621e995eb05e0edae
Cristian Acosta <79601880+Acosta-gh@users.noreply.github.com>
2026-01-21 15:14:31 -0300

    Cambios de config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/AuroraLabs/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
